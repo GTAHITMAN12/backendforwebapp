5f494ba8b09ef90c2b89f0d38307720f46eb6651
GTAHITMAN12 <natthawut24684@gmail.com>
2023-08-11 11:57:58 +0700

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderComponent } from './order/order.component';
+import { StoreComponent } from './store/store.component';
+import { DashbroadComponent } from './dashbroad/dashbroad.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent,
+    StoreComponent,
+    DashbroadComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
