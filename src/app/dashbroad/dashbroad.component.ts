49a77e0cf3c1b54dafd9a9c659349167bc921b2e
GTAHITMAN12 <natthawut24684@gmail.com>
2023-08-16 18:47:10 +0700

    COMPLTE

--- src/app/dashbroad/dashbroad.component.ts
+++ src/app/dashbroad/dashbroad.component.ts
@@ -16,7 +16,8 @@ export class DashbroadComponent {
   result: any;
   coinPrice: any;
   coinName: any;
-  
+  cointarget_sale:any;
+  coincount:any;
   constructor(
   private dashService: DashService
   ){ 
@@ -25,10 +26,13 @@ export class DashbroadComponent {
       this.dashService.getsale().subscribe(
         (res) => {      this.result = res;
           console.log(this.result);
-          this.coinPrice = this.result.map((coins: any) =>coins.target_sale);
+          this.coinPrice = this.result.map((coins: any) =>coins.price);
+          this.cointarget_sale = this.result.map((coins: any) =>coins.target_sale);
           this.coinName = this.result.map((coins: any) => coins.name);
+          this.coincount = this.result.map((coins: any) => coins.count);
           console.log(this.coinPrice);
           console.log(this.coinName);
+          console.log(this.cointarget_sale);
           this.chart = new Chart(this.canvas.nativeElement.getContext('2d'), {
             type: 'bar',
             data: {
@@ -39,6 +43,13 @@ export class DashbroadComponent {
                   borderColor: '#3e95cd',
                   label: 'Coin Price',
                   backgroundColor: 'rgba(93, 175, 89, 0.1)',
+                  borderWidth: 3,
+                },
+                {
+                  data: this.cointarget_sale,
+                  borderColor: '#3e95cd',
+                  label: 'Coin target_sale',
+                  backgroundColor: 'rgba(35, 228, 69, 0.8)',
                   borderWidth: 3,
                 },
               ],
